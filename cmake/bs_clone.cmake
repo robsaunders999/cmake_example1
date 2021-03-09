f9c3919a4a480e390f48272a0af62e5c2b78246e
Rob Saunders <rsaunders123@gmail.com>
2021-03-09 23:47:11 +0000

    add files

---
+++ cmake/bs_clone.cmake
@@ -0,0 +1,15 @@
+# bs_clone.cmake
+# Copy this file from the build system repo to the code repo.
+# Bootstrap's.
+
+# Clone repo
+include(FetchContent)
+FetchContent_Declare(buildsystem
+  GIT_REPOSITORY    git@github.com:robsaunders123/cmake_bs.git
+  GIT_TAG           master
+)
+FetchContent_MakeAvailable(buildsystem)
+
+# This places build system files in ${buildsystem_SOURCE_DIR}
+
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Examples
+
+To use the scripts copy bs_clone.cmake to your project dir, and include it 
+as shown in the example CMakeLists.txt file.
+
+This places all the build system scripts in ${buildsystem_SOURCE_DIR}
+
+
